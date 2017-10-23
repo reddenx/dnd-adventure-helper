3ff2bd2bc0ec214f8936ae84c26e17e37663fc41
sm_d3po <seanmarek@gmail.com>
2017-10-22 21:03:28 -0700

    think things are finally set up and routing correctly

---
+++ src/AdventureHelper.Website/Controllers/JournalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureHelper.Website.Controllers
+{
+    [RoutePrefix("journal")]
+    public class JournalController : Controller
+    {
+        [Route("")]
+        public ViewResult Journal()
+        {
+            return View();
+        }
+    }
+}
